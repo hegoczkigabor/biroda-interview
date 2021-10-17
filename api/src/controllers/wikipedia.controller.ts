24aa127262ae33ee439c7f86d91d723cfcad0e25
Hegoczki Gábor <hegoczkigabor@gmail.com>
2021-10-17 17:22:39 +0200

    simplify wikipedia controller

--- api/src/controllers/wikipedia.controller.ts
+++ api/src/controllers/wikipedia.controller.ts
@@ -3,9 +3,8 @@ import wikipediaService from "../services/wikipedia.service";
 const transformTitle = (title: string) => title.replace(/ /g, "_") + "_(film)";
 
 const getWikipediaOverviewByMovieTitle = async (title: string) => {
-  const wikiData = await wikipediaService.fetchWikiData(transformTitle(title));
-  const data: any = wikiData.data;
-  const pages = data.query.pages;
+  const { data } = await wikipediaService.fetchWikiData(transformTitle(title));
+  const pages = (<any>data).query.pages;
   const details = pages[Object.keys(pages)[0]];
   return details.extract;
 };
